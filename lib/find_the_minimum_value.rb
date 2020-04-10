2b90664fd483f83c5bba7b0be1ca5dd8d9236f72
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-04-10 03:06:38 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,9 +1,22 @@
+# def find_min_value(array)
+#   min = array[0]
+#   array.length.times do |i|
+#     if min>=array[i]
+#       min=array[i]
+#     end
+#   end
+#   min
+# end
+
+
 def find_min_value(array)
-  min = array[0]
-  array.length.times do |i|
-    if min>=array[i]
-      min=array[i]
-    end
-  end
-  min
+  counter = 0 
+  min_val = 0
+  while counter < array.length do 
+    if min_val == 0 || array[counter] < min_val
+      min_val = array[counter]
+    end 
+    counter += 1 
+  end 
+  min_val
 end
